d75d6d9d8725c232901a7f68f2d87b3b42817d3e
kalawang2000 <russell.limcangco@gmail.com>
2016-02-02 21:08:59 +0800

    up

--- assignment1.java
+++ assignment1.java
@@ -8,11 +8,20 @@ public class assignment1
 	
    public static void main(String args[]) throws Exception {
       int ara[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        
+      
+	  System.out.print("in right order  "); 
+	  for(int i=0 ; i<10 ; i++)
+	  {
+		  
+		  System.out.print(ara[i] + "  ");  
+		  
+	  }
+	 System.out.println();
+	 System.out.print("in right reverse order  "); 
 	  for(int i=9 ; i>-1 ; i--)
 	  {
 		  
-		  System.out.println(ara[i]);  
+		  System.out.print(ara[i] + "  ");  
 		  
 	  }
    
